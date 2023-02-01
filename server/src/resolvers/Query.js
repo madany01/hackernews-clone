2558a23395f6ebfe0cab36ca9dcf68f32e3a755d
Ahmad Madany <ahmad.madany.01@gmail.com>
2023-02-01 21:32:04 +0200

    Implement filtering pagination and sorting

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -2,8 +2,31 @@ function info() {
   return 'This is the API of a Hackernews Clone'
 }
 
-async function feed(root, args, { prisma }) {
-  return prisma.link.findMany()
+async function feed(root, { filter, skip, take, sort }, { prisma }) {
+  const where = !filter
+    ? {}
+    : {
+        OR: [
+          { url: { contains: filter } },
+          { description: { contains: filter } },
+        ],
+      }
+  const orderBy = !sort
+    ? undefined
+    : Object.entries(sort)
+        .filter(([, v]) => v !== null && v !== undefined)
+        .map(([f, v]) => ({ [f]: v }))
+
+  const links = await prisma.link.findMany({
+    where,
+    skip: skip ?? undefined,
+    take: take ?? undefined,
+    orderBy,
+  })
+
+  const count = links.length
+
+  return { links, count }
 }
 
 const Query = {
--- server/src/sandbox.js
+++ server/src/sandbox.js
@@ -3,15 +3,27 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 async function main() {
-  const link = await prisma.link.create({
-    data: {
-      url: 'www.howtographql.com',
-      description: 'Fullstack tutorial for GraphQL',
-    },
+  const u = await prisma.user.findFirst()
+  const l1 = await prisma.link.create({
+    data: { url: 'gql', description: '', authorId: u.id },
   })
-  console.log(link)
-  const links = await prisma.link.findMany({})
-  console.log(links)
+  const l2 = await prisma.link.create({
+    data: { url: '', description: 'gql', authorId: u.id },
+  })
+
+  try {
+    const res = await prisma.link.findMany({
+      orderBy: [{ description: 'desc' }, { createdAt: 'asc' }],
+    })
+    console.log(typeof res)
+    console.log(res)
+  } catch (e) {
+    await prisma.link.deleteMany({ where: { id: { in: [l1.id, l2.id] } } })
+    throw e
+  }
+
+  await prisma.link.deleteMany({ where: { id: { in: [l1.id, l2.id] } } })
+  // await prisma.user.deleteMany({ where: { id: { in: [u.id] } } })
 }
 
 ;(async () => {
